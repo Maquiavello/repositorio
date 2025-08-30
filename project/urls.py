f63fea019382455dc47c3309d09c9771779fa119
LittlePigmanPA <114762236+LittlePigmanPA@users.noreply.github.com>
2025-08-30 19:20:36 -0400

    Corrección final

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView # Importa RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Redirige la URL raíz a la URL de inicio de sesión.
+    path('', RedirectView.as_view(url='/app/login/', permanent=True)),
     path('app/', include('mi_app.urls')),
 ]
