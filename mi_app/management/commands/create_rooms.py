179cb551a7efaf860c972a3759a9d4ee1d959ad7
LittlePigmanPA <114762236+LittlePigmanPA@users.noreply.github.com>
2025-08-31 23:08:58 -0400

    Arreglado final

--- mi_app/management/commands/create_rooms.py
+++ mi_app/management/commands/create_rooms.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.core.management.base import BaseCommand
-from mi_app.models import Room
+from mi_app.models import Sala
 
 # Configura el logger para que los mensajes aparezcan en la consola de Render
 logger = logging.getLogger(__name__)
@@ -11,13 +11,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Lista de nombres de salas que deseas crear
-        room_names = ['Sala 1', 'Sala 2']
+        room_names = ['Sala 1', 'Sala 2', 'Sala 3']
         
         # Itera sobre la lista y crea cada sala si no existe ya
         for room_name in room_names:
             try:
                 # El método get_or_create es seguro para múltiples procesos y es atómico
-                sala, created = Room.objects.get_or_create(name=room_name)
+                sala, created = Sala.objects.get_or_create(name=room_name)
                 
                 if created:
                     self.stdout.write(self.style.SUCCESS(f'Sala "{sala.name}" creada exitosamente.'))
